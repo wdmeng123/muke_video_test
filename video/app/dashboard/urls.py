b72fef099c9b63e0797a07117f25c6d7cfd52094
wdmeng123 <330397529@qq.com>
2020-10-23 16:18:42 +0800

    添加mako模版初始化

--- video/app/dashboard/urls.py
+++ video/app/dashboard/urls.py
@@ -1,7 +1,8 @@
 # coding:utf-8
 
 from django.urls import path
+from .views.base import Base
 
 urlpatterns = [
-
+    path('base/', Base.as_view())
 ]
--- video/config/urls.py
+++ video/config/urls.py
@@ -3,10 +3,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from app.dashboard import urls as dashboard_urls
-from app.client import urls as clien_urls
+from app.client import urls as client_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include(dashboard_urls)),
-    path('client/', include(clien_urls)),
+    path('client/', include(client_urls)),
 ]
